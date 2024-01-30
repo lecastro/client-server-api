6d501ce4dbc007ed2338f1687f8e2aaf6c2bb2c2
Lucas Castro <lucas.castro1993@hotmail.com>
2024-01-30 10:23:14 -0300

    fix: save file

--- cmd/api-client/client.go
+++ cmd/api-client/client.go
@@ -2,13 +2,20 @@ package main
 
 import (
 	"context"
+	"encoding/json"
+	"fmt"
 	"io"
+	"log"
 	"net/http"
 	"os"
 	"time"
 
 	"github.com/lecastro/client-server-api/internal/helpers"
 )
+
+type dollarPrice struct {
+	Bid string `json:"bid"`
+}
 
 func main() {
 	ctx, cancel := context.WithTimeout(context.Background(), 1*time.Second)
@@ -29,4 +36,35 @@ func main() {
 	defer res.Body.Close()
 
 	io.Copy(os.Stdout, res.Body)
+
+	body, err := json.Marshal(res.Body)
+
+	if err != nil {
+		helpers.HandleError(err)
+	}
+
+	persistDollarPriceFile(body)
+}
+
+func persistDollarPriceFile(value []byte) {
+	var data map[string]dollarPrice
+
+	_ = json.Unmarshal(value, &data)
+
+	log.Println(data)
+	f, err := os.Create("./cotacao.txt")
+
+	if err != nil {
+		panic(err)
+	}
+
+	defer f.Close()
+
+	str := fmt.Sprintf("Dólar: {%0.2f}\n", data)
+
+	_, err = f.WriteString(str)
+
+	if err != nil {
+		panic(err)
+	}
 }
